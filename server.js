e70ed0d0f5f64166691e0f222ac387ab9e943d10
tejas <tejasgaware9403@gmail.com>
2025-07-20 13:23:17 +0530

    Database and server setup

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const connectDatabase = require("./config/db");
+const {} = require("./errors");
+const express = require("express");
+const app = express();
+
+// database
+connectDatabase();
+
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  console.log("on / route");
+});
+
+// error handling middlewares
+app.use();
+
+const start = async () => {
+  const PORT = process.env.PORT || 3000;
+  try {
+    app.listen(PORT, console.log(`Server listening on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
